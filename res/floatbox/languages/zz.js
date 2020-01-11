620daa31014a58d0d75f96808b0d72d81eb42c9c
Boris Melikian <boris@lariushin.org>
2020-01-11 02:31:58 +0200

    First publish

---
+++ res/floatbox/languages/zz.js
@@ -0,0 +1,19 @@
+fb.data.strings = [
+	"zz",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"Image %1 of %2",
+	"Image %1 of %2",
+	"Image %1 of %2",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"Pop-up content is blocked by this browser."
+];
